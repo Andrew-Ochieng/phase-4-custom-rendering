df17340d543d8c605d83d086e0070a32a263fe7d
Andrew-Ochieng <andrewomosh44@gmail.com>
2022-11-22 15:21:03 +0300

    Completed assignment

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -1,15 +1,30 @@
 class CheesesController < ApplicationController
 
   # GET /cheeses
+  # def index
+  #   cheeses = Cheese.all
+  #   render json: cheeses
+  # end
+
   def index
     cheeses = Cheese.all
-    render json: cheeses
+    render json: cheeses, except: [:created_at, :updated_at]
+
   end
 
   # GET /cheeses/:id
+  # def show
+  #   cheese = Cheese.find_by(id: params[:id])
+  #   render json: cheese
+  # end
+
   def show
     cheese = Cheese.find_by(id: params[:id])
-    render json: cheese
+    if cheese
+      render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
+    else
+      render json: {error: "Could not find"}, status: :not_found
+    end
   end
 
 end
